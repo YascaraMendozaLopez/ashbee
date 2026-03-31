780ed1ef8ccca30e8cf93d27d42f9a29530696e2
Yascara Marianed <mendozayo.gg@gmail.com>
2026-03-30 23:32:36 -0400

    fix/packs

--- src/data/spa.js
+++ src/data/spa.js
@@ -6,11 +6,11 @@ export const SERVICES = [
 ];
 
 export const PACKS = [
-  { name: "Anticelulitis", desc: "Tratamiento intensivo para mejorar la textura de la piel." },
-  { name: "Renuévate", desc: "Un respiro profundo para tu cuerpo y mente." },
-  { name: "Sculpt & Lift", desc: "Moldea y tonifica con tecnología de vanguardia." },
-  { name: "Post-operatorio", desc: "Cuidado especializado para tu recuperación." },
-  { name: "Glow Era", desc: "Luminosidad y frescura instantánea." },
-  { name: "Reset Corporal", desc: "Desintoxicación y equilibrio total." },
-  { name: "Post Parto", desc: "Recupera tu figura con suavidad y eficacia." }
+  { name: "Anticelulitis", desc: "Tratamiento intensivo para mejorar la textura de la piel.", image: "https://plus.unsplash.com/premium_photo-1661274108346-6e831c83d33e?q=80&w=687&auto=format&fit=crop" },
+  { name: "Renuévate", desc: "Un respiro profundo para tu cuerpo y mente.", image: "https://plus.unsplash.com/premium_photo-1683134297492-cce5fc6dae31?q=80&w=687&auto=format&fit=crop" },
+  { name: "Sculpt & Lift", desc: "Moldea y tonifica con tecnología de vanguardia.", image: "https://plus.unsplash.com/premium_photo-1682092334945-4ac6fda44868?q=80&w=687&auto=format&fit=crop" },
+  { name: "Post-operatorio", desc: "Cuidado especializado para tu recuperación.", image: "https://plus.unsplash.com/premium_photo-1661346142643-f106c8f88173?q=80&w=687&auto=format&fit=crop" },
+  { name: "Glow Era", desc: "Luminosidad y frescura instantánea.", image: "https://plus.unsplash.com/premium_photo-1682096988402-1ff0389fff5e?q=80&w=687&auto=format&fit=crop" },
+  { name: "Reset Corporal", desc: "Desintoxicación y equilibrio total.", image: "https://plus.unsplash.com/premium_photo-1670002426225-fff63cddad85?q=80&w=687&auto=format&fit=crop" },
+  { name: "Post Parto", desc: "Recupera tu figura con suavidad y eficacia.", image: "https://plus.unsplash.com/premium_photo-1663050773649-35d563f5903f?q=80&w=687&auto=format&fit=crops" }
 ];
